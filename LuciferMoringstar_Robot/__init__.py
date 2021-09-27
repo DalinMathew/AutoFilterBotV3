265a46792081b095e7370eea0ae9c5073a4d7f1b
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-27 23:58:44 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -29,5 +29,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-HI
+➥ Name :  Auto Filter With IMDB
+    
+➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
 """
