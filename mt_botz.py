f1f4664286f2e8b2465352576ec96e02215309c5
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-10-01 22:16:10 +0530

    Update mt_botz.py

--- mt_botz.py
+++ mt_botz.py
@@ -20,7 +20,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "AutoFilterBotV3"},
+            plugins={"root": "LuciferMoringstar_Robot"},
             sleep_threshold=5,
         )
 
