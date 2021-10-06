0c376e584d01b5ffe65e10c3e09765613f7da64d
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-10-06 08:36:08 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,11 +30,12 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](www.youtube.com/C/NaughtyPROFESSOR)
+➥ Creator : [Naughty PROFESSOR](
+https://youtube.com/c/NaughtyPROFESSOR)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me]()
+➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
 """
