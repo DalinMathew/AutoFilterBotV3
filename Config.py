adeb2af3749afd2f0fd51ee2dbf98ef80480b7a3
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-20 11:19:44 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -27,7 +27,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
+UPDATES = "https://t.me/ckofficial"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_2']
 DATABASE_NAME = environ['BOT_NAME']
