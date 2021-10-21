51d5f5396af04a4f218a625c5975d66955851379
MR TEM <92890384+Mrtem09@users.noreply.github.com>
2021-10-21 07:02:06 +0200

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -275,11 +275,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 )
                 return
         elif query.data == "help":
-            buttons = [
-                [
-                    InlineKeyboardButton('🔒Close', url=f'{TUTORIAL}')
-                ]
-                ]
+
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
         elif query.data == "about":
