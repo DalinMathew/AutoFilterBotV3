e9a875ec5504e5dd61245e02f22bb265396440b9
MR TEM <92890384+Mrtem09@users.noreply.github.com>
2021-10-21 09:09:04 +0200

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('⬆️ENJOY YOUR MOVIE⬆️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('⬆️ENJOY YOUR MOVIE⬆️', callback_data="help")
                     ]
                     ]
                 await bot.send_cached_media(
