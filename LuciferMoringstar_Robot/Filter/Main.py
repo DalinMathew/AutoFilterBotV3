b820e58f99b4d5e9f2f1609867f45a27f7dd9b2a
rohannn3x <94791480+rohannn3x@users.noreply.github.com>
2021-11-22 12:48:09 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -355,4 +355,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
+        await query.answer(" okad 👀",show_alert=True)
