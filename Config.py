54cd700d7d77157d5f19d2a6a32b283a0b1ab6ba
MH-BOTZ-b <103573719+MH-BOTZ-b@users.noreply.github.com>
2022-04-13 17:11:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -28,7 +28,7 @@ auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://t.me/file_search_updates"
-TOTURIAL = "https:))t.me/mh_Botz"
+TOTURIAL = "https:))t.me/mh_Botz/5"
 # MongoDB information
 DATABASE_URI = environ['DATABASE_2']
 DATABASE_NAME = environ['BOT_NAME']
